ad73df2e34623e8d08d79a5592c51e8c69333507
ymcheung <ymcheung.tw@me.com>
2022-07-14 01:04:09 +0800

    Add back sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid()]
+  integrations: [solid(), sitemap()],
+  site: 'https://moment.intersection.tw',
+  trailingSlash: 'never'
 });
