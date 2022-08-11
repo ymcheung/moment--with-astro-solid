44b8b2dc1e93d251f9713c4d2ea647a1cfb630cb
ymcheung <ymcheung.tw@me.com>
2022-08-11 20:57:15 +0800

    Add play button state

--- src/components/HeardItem.jsx
+++ src/components/HeardItem.jsx
@@ -1,67 +1,30 @@
+import { createSignal } from 'solid-js';
 import styles from '../styles/heardItem.module.css';
 
-export default function HeardItem({media}) {
-  const handleCreateIframe = (url, start) => {
-    const containers = document.querySelectorAll('.heard');
-    const songMeta = document.querySelector('.songMeta');
-    const expand = document.getElementsByClassName(`expandEmbed ${url}`);
-    const iframe = document.createElement('iframe');
-
-    if (document.body.querySelector('iframe')) {
-      const iframeToRemove = document.body.querySelector('iframe');
-
-      containers.forEach((container) => {
-        container.querySelectorAll('.expandEmbed').forEach(button => {
-          if (!button.hasAttribute('disabled')) return;
-          button.toggleAttribute('disabled');
-        });
-      });
-
-      document.body.removeChild(iframeToRemove);
-    }
-
-    const iframeAttributes = {
-      class: `${styles.player} ${url}`,
-      src: `https://www.youtube.com/embed/${url}?start=${start ? start : 0}`,
-      frameborder: 0,
-      allow: 'gyroscope; picture-in-picture',
-      allowfullscreen: true
-    };
-
-    const setAttributes = (element, attributes) => {
-      Object.keys(attributes).forEach(attr => {
-        element.setAttribute(attr, attributes[attr]);
-      });
-    }
-
-    setAttributes(iframe, iframeAttributes);
-    songMeta.parentNode.insertBefore(iframe, songMeta);
-
-    Array.from(expand).forEach(button => {
-      button.setAttribute('disabled', 'disabled');
-    });
-  };
-
+export default function HeardItem({ media }) {
   return (
     <ul class={styles.mediaList}>
       {
-        media.map(({ name, year, url, start }) =>
-          <li className={styles.listItem}>
-            <span className={styles.mediaName}>{name}</span>
-            <span className={styles.mediaYear}>{year}</span>
-            {url &&
-              <button
-                className={`${styles.expand} expandEmbed ${url}`}
-                type="button"
-                onClick={() =>{handleCreateIframe(url, start)}}
-                data-splitbee-event="點擊影片"
-                data-splitbee-event-type={name}
-              >
-                <img className={styles.iconYoutube} src="/youtube.svg" alt="播放影片" />
-              </button>
-            }
-          </li>
-        )
+        media.map(({ name, year, url, start }) => {
+          return (
+            <li className={styles.listItem}>
+              <span className={styles.mediaName}>{name}</span>
+              <span className={styles.mediaYear}>{year}</span>
+              {url &&
+                <button
+                  className={styles.expand}
+                  type="button"
+                  // onClick={() =>{handleCreateIframe(url, start)}}
+                  // disabled={}
+                  data-splitbee-event="點擊影片"
+                  data-splitbee-event-type={name}
+                >
+                  <img className={styles.iconYoutube} src="/youtube.svg" alt="播放影片" />
+                </button>
+              }
+            </li>
+          )
+        })
       }
     </ul>
   );
