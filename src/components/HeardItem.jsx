782be3efc92f4e4260008e16dfd5c05652295588
ymcheung <ymcheung.tw@me.com>
2022-08-11 20:58:19 +0800

    Rearrange HeardItem

--- src/components/HeardItem.jsx
+++ src/components/HeardItem.jsx
@@ -1,31 +1,23 @@
 import { createSignal } from 'solid-js';
 import styles from '../styles/heardItem.module.css';
 
-export default function HeardItem({ media }) {
+export default function HeardItem({ name, year, url, start }) {
   return (
-    <ul class={styles.mediaList}>
-      {
-        media.map(({ name, year, url, start }) => {
-          return (
-            <li className={styles.listItem}>
-              <span className={styles.mediaName}>{name}</span>
-              <span className={styles.mediaYear}>{year}</span>
-              {url &&
-                <button
-                  className={styles.expand}
-                  type="button"
-                  // onClick={() =>{handleCreateIframe(url, start)}}
-                  // disabled={}
-                  data-splitbee-event="點擊影片"
-                  data-splitbee-event-type={name}
-                >
-                  <img className={styles.iconYoutube} src="/youtube.svg" alt="播放影片" />
-                </button>
-              }
-            </li>
-          )
-        })
+    <li className={styles.listItem}>
+      <span className={styles.mediaName}>{name}</span>
+      <span className={styles.mediaYear}>{year}</span>
+      {url &&
+        <button
+          className={styles.expand}
+          type="button"
+          // onClick={() =>{handleCreateIframe(url, start)}}
+          // disabled={}
+          data-splitbee-event="點擊影片"
+          data-splitbee-event-type={name}
+        >
+          <img className={styles.iconYoutube} src="/youtube.svg" alt="播放影片" />
+        </button>
       }
-    </ul>
-  );
+    </li>
+  )
 }
