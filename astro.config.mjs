4b60a2f0597a0102c7da33499603b7323a21a4c0
ymcheung <ymcheung.tw@me.com>
2022-09-14 09:14:34 +0800

    Update bohemian rhapsody & stayin alive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 import sitemap from '@astrojs/sitemap';
-
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
